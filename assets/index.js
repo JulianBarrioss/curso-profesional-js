f7642c39ce53ef0d0516ad4f770d7c8f5fb94a6d
Julian Barrios <julianbarrios0821@gmail.com>
2021-08-03 17:58:01 -0600

    js file added

---
+++ assets/index.js
@@ -0,0 +1,14 @@
+const video = document.querySelector("video");
+const button = document.querySelector("button");
+
+function mediaPlayer(config){
+  this.media = config.el
+}
+
+mediaPlayer.prototype.play = function(){
+  this.media.play();
+};
+
+const player = new mediaPlayer({ el: video });
+
+button.onclick = () => video.play();
